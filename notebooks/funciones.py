de0e214f2946078f0e171bfa1fdf050fec394aa3
bperezlovisolo <b.perezlovisolo@gmail.com>
2026-02-10 14:07:09 +0100

    subo actualizaciones

--- notebooks/funciones.py
+++ notebooks/funciones.py
@@ -1,1 +1,43 @@
 import pandas as pd
+
+def informe(x):
+    print("Información del DataFrame:")
+    print(x.info())
+    print("Descripción estadística:")
+    print(x.describe())
+    print("Valores nulos:")
+    print(x.isnull().sum())
+    print("Tipos de datos:")
+    print(x.dtypes)
+    
+    print("Primeras 5 filas:")
+    print(x.head())
+    print("Últimas 5 filas:")
+    print(x.tail())
+    print("Forma del DataFrame:")
+    print(x.shape)
+    print("columnas")
+    print(x.columns)
+    print("numericos")
+    print(x.select_dtypes(include=["number"]).columns)
+
+def datos(x):
+    print("datos nulos")
+    print(x.isnull().sum())
+    print("datos totales")
+    print(x.count())   
+    print("porcentaje de nulos")
+    print(x.isnull().sum() / x.shape[0]*100)
+    print("datos duplicados")
+    print(x.duplicated().sum())
+    print(x.duplicated().any())
+
+
+def limpiar_nulos(x):
+    df = x.copy()
+    df = x.dropna()
+    return df
+
+
+def drop_columns (df, columns):
+    df.drop(columns, axis=1, inplace=True)
