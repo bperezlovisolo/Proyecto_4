05c66f6f71e9f3799eac8e3757f9bbfef8688e20
eliomartinezcastano-stack <eliomartinezcastano@gmail.com>
2026-02-18 18:04:55 +0100

    He añadido funciones y comprobado que funcione

--- notebooks/funciones.py
+++ notebooks/funciones.py
@@ -41,3 +41,37 @@ def limpiar_nulos(x):
 
 def drop_columns (df, columns):
     df.drop(columns, axis=1, inplace=True)
+
+
+def clean_demo_data(df):
+    #Renombramos columnas
+    df = df.rename(columns={
+        'clnt_tenure_yr': 'tenure_year',
+        'clnt_tenure_mnth': 'tenure_month', 
+        'clnt_age': 'age', 
+        'gendr': 'gender', 
+        'num_accts': 'num_accounts'})
+    
+    #Convertimos y redondeamos
+    cols_to_int = ['num_accounts', 'calls_6_mnth', 'logons_6_mnth', 'tenure_year', 'tenure_month', 'age']
+    for col in cols_to_int:
+        df[col] = df[col].round(0).astype('Int64')
+    
+    #Limpiamos género
+    df['gender'] = (df['gender'].astype("string").str.strip().str.upper().replace("X", "U").fillna("U"))
+
+    return df
+
+
+def categorize_columns(df):
+    #Creamos categoría edad
+    age_bins = [0, 18, 35, 60, df["age"].max()]
+    age_labels = ['Under 18', 'Young Adult', 'Adult', 'Senior']
+    df['age_category'] = pd.cut(df['age'], bins=age_bins, labels=age_labels, include_lowest=True)
+
+    #Creamos categoría antigüedad
+    tenure_bins = [0, 5, 10, 20, df["tenure_year"].max()]
+    tenure_labels = ['New', 'Intermediate', 'Long Term', 'Veteran']
+    df['tenure_category'] = pd.cut(df['tenure_year'], bins=tenure_bins, labels=tenure_labels, include_lowest=True)
+
+    return df
